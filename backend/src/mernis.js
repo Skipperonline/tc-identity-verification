234f7ed5743d1a2276c124d8aae6ddb202c14ead
Sinan K. <75752768+sinan-keskin@users.noreply.github.com>
2025-11-28 23:27:15 +0300

    Update mernis.js

--- backend/src/mernis.js
+++ backend/src/mernis.js
@@ -1,1 +1,6 @@
+async function verifyWithMernis({ tckn, firstName, lastName, birthYear }) {
+  // Eğer ki yetkili bir kurumsanız bu entegrasyonu kendi api bilgileriniz ile doldurmalısınız.
+  return false;
+}
 
+module.exports = { verifyWithMernis };
